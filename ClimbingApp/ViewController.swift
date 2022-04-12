e5b2b3425aeb57d86897a77e1177b1b4a28a013e
Taiga Morikawa <motantanmen28@gmail.com>
2022-04-12 21:53:05 +0900

    Initial Commit

---
+++ ClimbingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimbingApp
+//
+//  Created by 森川大雅 on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

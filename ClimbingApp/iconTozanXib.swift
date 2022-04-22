143a6425bd8e1fdf8cc89ca77b973d590e5f23af
Forest-River-Tiger <motantanmen.github@gmail.com>
2022-04-23 01:04:09 +0900

    feat:上部アイコン用のXib追加

---
+++ ClimbingApp/iconTozanXib.swift
@@ -0,0 +1,33 @@
+//
+//  iconTozanXib.swift
+//  ClimbingApp
+//
+//  Created by 森川大雅 on 2022/04/23.
+//
+
+import UIKit
+
+class iconTozanXib: UIView {
+
+    // コードから呼び出す際に使用
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    // Storyboardから利用する際に使用
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadNib()
+    }
+    
+    // 上記どちらのinitからも使用される共通関数でxibファイルを呼び出す
+    func loadNib() {
+        // 第1引数のnameにはxibファイル名
+        // ownerはself固定
+        // optionsはnil
+        let view = Bundle.main.loadNibNamed("iconTozanView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
